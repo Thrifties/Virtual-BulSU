226a15e5f9e01f07a94a0d45f8965497fb4b8888
Thrifties <90311744+Thrifties@users.noreply.github.com>
2023-11-07 20:46:22 +0800

    Remove announcement list modal from test.php.

--- test.php
+++ test.php
@@ -266,18 +266,6 @@ require "connect.php";
       </div>
     </div>
 
-    <div class="modal fade" id="announcementListModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog modal-xl">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-      </div>
-    </div>
-  </div>
-</div>
 
     <footer class="footer">
       <!-- &copy; 2023 Bulacan State University.  -->All rights reserved.
