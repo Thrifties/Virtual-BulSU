d2ac10e7f3c8bc7c864f632a9d542022e46766f4
Thrifties <90311744+Thrifties@users.noreply.github.com>
2023-11-12 10:48:14 +0800

    Update CSS and database connection details, and remove unused code.

--- add_admin_details.php
+++ add_admin_details.php
@@ -19,14 +19,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if ($currentAdminLevel == "super_admin") {
         $facultyId = $_POST["addFacultyId"];
         $password = $_POST["addPassword"];
-        $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
         $firstName = $_POST["firstName"];
         $middleName = $_POST["middleName"];
         $lastName = $_POST["lastName"];
         $campus = $_POST["addCampus"];
         $email = $_POST["addEmail"];
         $phone = $_POST["addPhone"];
-        $query = "INSERT INTO `campus_admin`(`faculty_id`, `first_name`, `middle_name`, `last_name`, `campus`, `email`, `contact_num`, `password`) VALUES ('$facultyId','$firstName','$middleName','$lastName','$campus','$email','$phone','$hashedPassword')";
+        $query = "INSERT INTO `campus_admin`(`faculty_id`, `first_name`, `middle_name`, `last_name`, `campus`, `email`, `contact_num`, `password`) VALUES ('$facultyId','$firstName','$middleName','$lastName','$campus','$email','$phone','$password')";
         if (mysqli_query($con, $query)) {
             // Return a success message (you can customize the response as needed)
             echo "Admin added successfully!";
@@ -45,7 +44,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $college = $_POST["addCollege"];
         $email = $_POST["addEmail"];
         $phone = $_POST["addPhone"];
-        $query = "INSERT INTO `college_admin`(`faculty_id`, `first_name`, `middle_name`, `last_name`, `campus`, `college`, `email`, `contact_num`, `password`) VALUES ('$facultyId','$firstName','$middleName','$lastName','$currentAdminCampus','$college','$email','$phone','$hashedPassword')";
+        $query = "INSERT INTO `college_admin`(`faculty_id`, `first_name`, `middle_name`, `last_name`, `campus`, `college`, `email`, `contact_num`, `password`) VALUES ('$facultyId','$firstName','$middleName','$lastName','$currentAdminCampus','$college','$email','$phone','$password')";
         if (mysqli_query($con, $query)) {
             // Return a success message (you can customize the response as needed)
             echo "Admin added successfully!";
